7db97d3c8ae3d7175031b0c79788c98111b3795f
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-11-10 22:40:53 -0500

    12.6 checkpoint

--- chp12-minigrep/src/main.rs
+++ chp12-minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::process;
 use minigrep::Config;
 
 /*
- * "It’s better to have a working program that’s a bit inefficient 
+ * "It’s better to have a working program that’s a bit inefficient
  * than to try to hyperoptimize code on your first pass."
  */
 
@@ -12,12 +12,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem encountered while parsing arguments: {err}");
+        eprintln!("Problem encountered while parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Something broke: {e}");
+        eprintln!("Something broke: {e}");
         process::exit(1);
     };
 }
