0255cb0934c578912f0fd934b159a28d3e8a2d0f
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-11-10 21:54:10 -0500

    12.4 checkpoint

--- chp12-minigrep/src/lib.rs
+++ chp12-minigrep/src/lib.rs
@@ -19,17 +19,41 @@ impl Config {
             }
             _ => unreachable!()
         };
-        
+
         result
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Searching for {} in file {}", config.query, config.file_path);
-    
-    let contents = fs::read_to_string(config.file_path)?; 
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("File contents:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
-}       
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let matches: Vec<&'a str> = contents.lines()
+        .filter(|line| line.contains(query))
+        .collect();
+
+    matches
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
