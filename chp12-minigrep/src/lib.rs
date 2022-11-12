bb883ad7ec2ede8662168493204347f74d79cdf0
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-11-11 21:50:46 -0500

    13.3 checkpoint

--- chp12-minigrep/src/lib.rs
+++ chp12-minigrep/src/lib.rs
@@ -9,25 +9,38 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        let result = match args.len() {
-            1..=2 => Err("Not enough arguments provided!"),
-            4.. => Err("Too many arguments provided!"),
-            3 => {
-                Ok(Config {
-                    query: args[1].clone(),
-                    file_path: args[2].clone(),
-                    ignore_case: env::var("IGNORE_CASE").is_ok()
-                })
-            }
-            _ => unreachable!()
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
+
+        // args.count() takes ownership and consumes the iterator,
+        // so I trashed my arg count checks
+        // See: https://doc.rust-lang.org/nightly/std/iter/trait.Iterator.html#method.count
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
         };
 
-        result
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(
+    config: Config
+) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
     let search_results: Vec<&str> = match config.ignore_case {
@@ -42,23 +55,25 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let matches: Vec<&'a str> = contents.lines()
+pub fn search<'a>(
+    query: &str, contents: &'a str
+) -> Vec<&'a str> {
+    contents
+        .lines()
         .filter(|line| line.contains(query))
-        .collect();
-
-    matches
+        .collect()
 }
 
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let matches: Vec<&'a str> = contents.lines()
+pub fn search_case_insensitive<'a>(
+    query: &str, contents: &'a str
+) -> Vec<&'a str> {
+    contents
+        .lines()
         .filter(|line|
             line.to_lowercase()
                 .contains(&query.to_lowercase())
         )
-        .collect();
-
-    matches
+        .collect()
 }
 
 #[cfg(test)]
